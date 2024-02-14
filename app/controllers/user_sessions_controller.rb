2849f180206f7e3a4aea44b5c59fc6bc142fcf67
yanger <kanayaofficial1129@gmail.com>
2024-02-14 19:53:25 +0900

    add decorator

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,7 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path, status: :see_other, flash: { success: t('.success') }
+      redirect_to root_path, status: :see_other, flash: { success: t('.success') }
     else
       flash.now[:danger] = t('.failure')
       render :new, status: :unauthorized
