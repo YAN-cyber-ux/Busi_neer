3b1eff11b3a0090bbbdbfc557b0373baec0a946c
yanger <kanayaofficial1129@gmail.com>
2024-02-15 19:59:05 +0900

    add login and timezone

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module BusiNeer
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.generators do |g|
       g.skip_routes true
       g.assets false
