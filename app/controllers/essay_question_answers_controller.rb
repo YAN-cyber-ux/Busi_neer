938c80a7a47050895a16733a097a2137bfe5b298
yanger <kanayaofficial1129@gmail.com>
2024-02-16 19:57:52 +0900

    modify routes and controller

--- app/controllers/essay_question_answers_controller.rb
+++ app/controllers/essay_question_answers_controller.rb
@@ -1,4 +1,6 @@
 class EssayQuestionAnswersController < ApplicationController
+  before_action :find_essay_question_answer, only: %i[edit update destroy]
+
   def index
     @essay_question_answers = EssayQuestionAnswer.all.includes(:user).order(created_at: :desc)
   end
@@ -21,7 +23,27 @@ class EssayQuestionAnswersController < ApplicationController
     @essay_question_answer = EssayQuestionAnswer.find(params[:id])
   end
 
+  def edit; end
+  
+    def update
+      if @essay_question_answer.update(essay_question_answer_params)
+        redirect_to essay_question_answers_path, status: :see_other, flash: { success: t('defaults.message.updated', item: EssayQuestionAnswer.model_name.human) }
+      else
+        flash.now[:danger] = t('defaults.message.not_updated', item: EssayQuestionAnswer.model_name.human)
+        render :edit, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @essay_question_answer.destroy!
+      redirect_to essay_question_answers_path, status: :see_other, flash: { success: t('defaults.message.deleted', item: EssayQuestionAnswer.model_name.human) }
+    end
+
   private
+
+  def find_essay_question_answer
+    @board = current_user.essay_question_answers.find(params[:id])
+  end
 
   def essay_question_answer_params
     params.require(:essay_question_answer).permit(:title, :body)
