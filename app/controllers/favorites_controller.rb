462cb6108cc9a1753d13d1a3ec85c2ff6f13fc70
yanger <kanayaofficial1129@gmail.com>
2024-02-18 21:15:21 +0900

    modify relation

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,8 +6,7 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    #デバッグが必要
-    essay_question_answer = current_user.favorites.find(params[:id]).essay_question_answer
+    essay_question_answer = current_user.favorites.find_by(essay_question_answer_id: params[:id]).essay_question_answer #find(:params[:id])にするとエラーが出る
     current_user.unfavorite(essay_question_answer)
     redirect_to essay_question_answers_path, success: t('.success'), status: :see_other
   end
